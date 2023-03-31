d6e1092fc622179e12f26d0b421d54e31800e292
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-31 21:01:20 +0900

    refactored an example

--- example/refactor/test/argumentsPresence/unitTests/eqv_l_l.f90
+++ example/refactor/test/argumentsPresence/unitTests/eqv_l_l.f90
@@ -15,18 +15,20 @@ contains
         type(error_type), allocatable, intent(out) :: error
         !! error handler
 
-        type(test_parameter_type), allocatable :: params(:)
+        type(parameterization_spec_type) :: spec
         type(test_results_type) :: results
 
-        params = [ &
-                   new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true",     expected="is_equal=true") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true",   expected="is_equal=true") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false",   expected="is_equal=true") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false", expected="is_equal=true") &
-                 ] !&
-
-        call run_test_cases_l2l2(params, results)
+        spec = new_parameterization_spec( &
+               [ &
+                 new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true"    , expected="is_equal=true") &
+               , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true"  , expected="is_equal=true") &
+               , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false"  , expected="is_equal=true") &
+               , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false", expected="is_equal=true") &
+               ]) !&
+        call results%construct(spec)
+        call run_test_cases_l2l2(spec, results)
         call check(error, results%all_cases_successful(), results%get_summary_message())
+        call results%destruct()
     end subroutine eqvLL_should_return_true_when_2_arrays_have_same_values
 
     subroutine eqvLL_should_return_false_when_2_arrays_have_diffelent_values(error)
@@ -34,49 +36,53 @@ contains
         type(error_type), allocatable, intent(out) :: error
         !! error handler
 
-        type(test_parameter_type), allocatable :: params(:)
+        type(parameterization_spec_type) :: spec
         type(test_results_type) :: results
 
-        params = [ &
-                 new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false",  expected="is_equal=false") &
-                 ] !&
+        spec = new_parameterization_spec( &
+               [ &
+                 new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true"  , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false"  , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false" , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true"  , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false" , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false", expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true"  , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true" , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false", expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true" , expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true", expected="is_equal=false") &
+               , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false", expected="is_equal=false") &
+               ]) !&
+        call results%construct(spec)
 
-        call run_test_cases_l2l2(params, results)
+        call run_test_cases_l2l2(spec, results)
         call check(error, results%all_cases_successful(), results%get_summary_message())
+
+        call results%destruct()
     end subroutine eqvLL_should_return_false_when_2_arrays_have_diffelent_values
 
-    subroutine run_test_cases_l2l2(params, results)
+    subroutine run_test_cases_l2l2(spec, results)
         implicit none
-        type(test_parameter_type), intent(in) :: params(:)
+        type(parameterization_spec_type), intent(in) :: spec
         type(test_results_type), intent(inout) :: results
 
         logical :: lhs(2), rhs(2), is_equal
         namelist /arguments/ lhs, rhs
         namelist /expected/ is_equal
 
+        type(test_parameter_type) :: param
         character(:), allocatable :: case_name
         logical :: actual
         integer(int32) :: case
 
-        call results%construct(params)
-
         do case = 1, results%get_number_of_test_cases()
-            read (unit=params(case)%expected_namelist, nml=expected)
-            read (unit=params(case)%arguments_namelist, nml=arguments)
+            param = spec%get_test_parameter_in(case)
+            read (unit=param%expected_namelist, nml=expected)
+            read (unit=param%arguments_namelist, nml=arguments)
 
-            case_name = "eqv_logical_logical() should return "//params(case)%expected()// &
-                        " when compare "//params(case)%arguments()
+            case_name = "eqv_logical_logical() should return "//param%expected()// &
+                        " when compare "//param%arguments()
 
             actual = (lhs == rhs)
 
@@ -92,69 +98,72 @@ contains
         type(error_type), allocatable, intent(out) :: error
             !! error handler
 
-        type(test_parameter_type), allocatable :: params(:)
+        type(parameterization_spec_type) :: spec
         type(test_results_type) :: results
 
-        params = [ &
-                   new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true,true",      expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true,true",     expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false,true",     expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true,false",     expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true,false",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false,false",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true,true",     expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true,false",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true,true",     expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true,false",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true,true",    expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false,true",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false,true",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true,false",   expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false,false",  expected="is_equal=false") &
-                 , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false,false", expected="is_equal=false") &
-                 ] !&
+        spec = new_parameterization_spec( &
+               [ &
+                  new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true,true",      expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true,true",     expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false,true",     expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,true,false",     expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,true,false",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=true,false,false",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,true rhs(:)=false,false,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true,true",     expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false,true",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,true,false",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,true,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=true,false,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,true rhs(:)=false,false,false",  expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true,true",     expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false,true",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,true,false",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,true,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=true,false,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=true,false rhs(:)=false,false,false",  expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true,true",    expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true,true",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false,true",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false,true",  expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,true,false",   expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,true,false",  expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=true,false,false",  expected="is_equal=false") &
+                , new_test_parameter(arguments="lhs(:)=false,false rhs(:)=false,false,false", expected="is_equal=false") &
+                ]) !&
+        call results%construct(spec)
 
-        call run_test_cases(params, results)
+        call run_test_cases(spec, results)
         call check(error, results%all_cases_successful(), results%get_summary_message())
 
+        call results%destruct()
     contains
-        subroutine run_test_cases(params, results)
+        subroutine run_test_cases(spec, results)
             implicit none
-            type(test_parameter_type), intent(in) :: params(:)
+            type(parameterization_spec_type), intent(in) :: spec
             type(test_results_type), intent(inout) :: results
 
             logical :: lhs(2), rhs(3), is_equal
             namelist /arguments/ lhs, rhs
             namelist /expected/ is_equal
 
+            type(test_parameter_type) :: param
             character(:), allocatable :: case_name
             logical :: actual
             integer(int32) :: case
 
-            call results%construct(params)
-
             do case = 1, results%get_number_of_test_cases()
-                read (unit=params(case)%expected_namelist, nml=expected)
-                read (unit=params(case)%arguments_namelist, nml=arguments)
+                param = spec%get_test_parameter_in(case)
+                read (unit=param%expected_namelist, nml=expected)
+                read (unit=param%arguments_namelist, nml=arguments)
 
-                case_name = "eqv_logical_logical() should return "//params(case)%expected()// &
-                            " when compare "//params(case)%arguments()
+                case_name = "eqv_logical_logical() should return "//param%expected()// &
+                            " when compare "//param%arguments()
 
                 actual = (lhs == rhs)
 
