7344a99f867b063c3750b2d8645e4a0b74928fb8
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-31 18:43:44 +0900

    added modules to `per_funnel`

--- src/parFunnel.f90
+++ src/parFunnel.f90
@@ -2,6 +2,8 @@ module par_funnel
     use :: type_testParameter
     use :: type_argumentsPresence
     use :: type_testResults
+    use :: type_argument
+    use :: type_parameterizationSpec
     implicit none
     public
 end module par_funnel
