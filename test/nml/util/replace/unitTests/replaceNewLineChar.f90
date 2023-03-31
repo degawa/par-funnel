d09d86b749ba24d83d15b306bf1719eff5e01811
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-31 22:07:03 +0900

    added unit test

---
+++ test/nml/util/replace/unitTests/replaceNewLineChar.f90
@@ -0,0 +1,48 @@
+module test_nmlUtil_unitTests_replace_newLineChar
+    use, intrinsic :: iso_fortran_env
+    use :: testdrive, only:error_type, check
+    use :: testdrive_util, only:occurred, to_string
+    use :: strings_enclose
+    use :: nml_util_replace
+    implicit none
+    private
+    public :: replNLChar_should_return_string_that_replaces_char_with_mark
+
+contains
+    subroutine replNLChar_should_return_string_that_replaces_char_with_mark(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+        !! error handler
+
+        character(:), allocatable :: str_namelist, expected_namelist, actual_namelist
+
+        call setup(str_namelist, expected_namelist)
+
+        actual_namelist = replace_new_line_char(str_namelist)
+
+        call check(error, len(actual_namelist) == len(expected_namelist), &
+                   "expected namelist length "//to_string(len(expected_namelist)) &
+                   //", but got "//to_string(len(actual_namelist)))
+        if (occurred(error)) return
+
+        call check(error, actual_namelist == expected_namelist, &
+                   "expected "//enclose(expected_namelist, '"')//", "// &
+                   "but got "//enclose(actual_namelist, '"'))
+
+        call teardown(str_namelist, expected_namelist, actual_namelist)
+    contains
+        !
+        subroutine setup(str_namelist, expected_namelist)
+            character(:), allocatable, intent(out) :: str_namelist, expected_namelist
+            str_namelist = "&namelist_group_name input1=1 "//new_line(" ")//"input2=10 "//new_line(" ")//"input3=4 /"
+            expected_namelist = "&namelist_group_name input1=1 \ninput2=10 \ninput3=4 /"
+        end subroutine setup
+        !
+        subroutine teardown(str_namelist, expected_namelist, actual_namelist)
+            character(:), allocatable, intent(inout) :: str_namelist, expected_namelist, actual_namelist
+            deallocate (str_namelist)
+            deallocate (expected_namelist)
+            deallocate (actual_namelist)
+        end subroutine teardown
+    end subroutine replNLChar_should_return_string_that_replaces_char_with_mark
+end module test_nmlUtil_unitTests_replace_newLineChar
